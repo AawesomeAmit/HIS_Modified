2419b77b5ef2c2251e9f03d0554f74fa1c95cb18
HID-16 <aamitonline@gmail.com>
2020-05-26 12:07:22 +0530

    26 May 2020

--- app/src/main/java/com/trueform/era/his/Fragment/InputVital.java
+++ app/src/main/java/com/trueform/era/his/Fragment/InputVital.java
@@ -97,7 +97,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
     private SimpleDateFormat format2;
     private static String date = "";
     private Date today = new Date();
-    private int mYear = 0, mMonth = 0, mDay = 0, mHour=0, mMinute=0;
+    private int mYear = 0, mMonth = 0, mDay = 0, mHour = 0, mMinute = 0;
     private RecyclerView rvVitalDisplay;
     //static RecyclerView rvAddVital;
     private ArrayAdapter<VitalList> adapter;
@@ -137,27 +137,27 @@ public class InputVital extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_input_vital, container, false);
-        spnVital=view.findViewById(R.id.spnVital);
-        txtUnit=view.findViewById(R.id.txtUnit);
-        txtVoice=view.findViewById(R.id.txtVoice);
-        edtVal=view.findViewById(R.id.edtQty);
+        View view = inflater.inflate(R.layout.fragment_input_vital, container, false);
+        spnVital = view.findViewById(R.id.spnVital);
+        txtUnit = view.findViewById(R.id.txtUnit);
+        txtVoice = view.findViewById(R.id.txtVoice);
+        edtVal = view.findViewById(R.id.edtQty);
         Toolbar toolbar = Objects.requireNonNull(getActivity()).findViewById(R.id.toolbar);
-        spnConsultant=toolbar.findViewById(R.id.spnConsultant);
-        rvVitalDisplay=view.findViewById(R.id.rvVitalDisplay);
+        spnConsultant = toolbar.findViewById(R.id.spnConsultant);
+        rvVitalDisplay = view.findViewById(R.id.rvVitalDisplay);
         TextView btnSave = view.findViewById(R.id.btnSave);
-        txtDate=view.findViewById(R.id.txtDate);
-        txtTime=view.findViewById(R.id.txtTime);
+        txtDate = view.findViewById(R.id.txtDate);
+        txtTime = view.findViewById(R.id.txtTime);
         txtDate.setOnClickListener(this);
         txtTime.setOnClickListener(this);
         txtVoice.setOnClickListener(this);
-        context =view.getContext();
+        context = view.getContext();
         c = Calendar.getInstance();
         Utils.showRequestDialog(context);
         mYear = c.get(Calendar.YEAR);
         mMonth = c.get(Calendar.MONTH);
         mDay = c.get(Calendar.DAY_OF_MONTH);
-        date = c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH)+1) + "/" + c.get(Calendar.YEAR);
+        date = c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH) + 1) + "/" + c.get(Calendar.YEAR);
         txtTime.setText(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + " " + c.get(Calendar.AM_PM));
         format1 = new SimpleDateFormat("dd/MM/yyyy");
         format2 = new SimpleDateFormat("hh:mm a");
@@ -166,7 +166,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
         txtTime.setText(format2.format(today));
         //txtDate.setText(date);
         //vitalLists1=new ArrayList<>();
-        vitalLists=new ArrayList<>();
+        vitalLists = new ArrayList<>();
         rvVitalDisplay.setLayoutManager(new LinearLayoutManager(context));
         //rvAddVital.setLayoutManager(new LinearLayoutManager(context));
         bind();
@@ -183,7 +183,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
             }
         });*/
         vitalLists.add(0, new VitalList(0, "Select", "", ""));
-        Call<List<VitalList>> call= RetrofitClient.getInstance().getApi().getVitalMaster(SharedPrefManager.getInstance(context).getUser().getAccessToken(), SharedPrefManager.getInstance(context).getUser().getUserid().toString());
+        Call<List<VitalList>> call = RetrofitClient.getInstance().getApi().getVitalMaster(SharedPrefManager.getInstance(context).getUser().getAccessToken(), SharedPrefManager.getInstance(context).getUser().getUserid().toString());
         call.enqueue(new Callback<List<VitalList>>() {
             @Override
             public void onResponse(Call<List<VitalList>> call, Response<List<VitalList>> response) {
@@ -205,17 +205,16 @@ public class InputVital extends Fragment implements View.OnClickListener {
             }
         });
         btnSave.setOnClickListener(view1 -> {
-            if (SharedPrefManager.getInstance(context).getUser().getDesigid()==1) {
+            if (SharedPrefManager.getInstance(context).getUser().getDesigid() == 1) {
                 saveVitals(SharedPrefManager.getInstance(context).getUser().getUserid());
-            }
-            else if (SharedPrefManager.getInstance(context).getUser().getDesigid()!=1 && spnConsultant.getSelectedItemPosition() != 0) {
+            } else if (SharedPrefManager.getInstance(context).getUser().getDesigid() != 1 && spnConsultant.getSelectedItemPosition() != 0) {
                 saveVitals(SharedPrefManager.getInstance(context).getConsultantList().get(spnConsultant.getSelectedItemPosition()).getUserid());
             } else Toast.makeText(context, "Consultant name required!", Toast.LENGTH_LONG).show();
         });
         spnVital.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i!=0) {
+                if (i != 0) {
                     txtUnit.setText(vitalLists.get(i).getUnit());
                     vital = vitalLists.get(i);
                 }
@@ -229,7 +228,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
         return view;
     }
 
-    private void recordingPopup(){
+    private void recordingPopup() {
         View popupView = getLayoutInflater().inflate(R.layout.popup_vital_recording, null);
         final PopupWindow popupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, true);
         LinearLayout lLayout = popupView.findViewById(R.id.lLayout);
@@ -252,7 +251,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
         popupWindow.showAtLocation(lLayout, Gravity.CENTER, 0, 0);
         btnClose.setOnClickListener(view -> popupWindow.dismiss());
         startbtn.setOnClickListener(v -> {
-            if(CheckPermissions()) {
+            if (CheckPermissions()) {
                 stopbtn.setEnabled(true);
                 startbtn.setEnabled(false);
                 playbtn.setEnabled(false);
@@ -262,7 +261,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
                 stopbtn.setVisibility(View.VISIBLE);
                 String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
                 mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-                mFileName += "/"+timeStamp+"_VitalRecording.mp3";
+                mFileName += "/" + timeStamp + "_VitalRecording.mp3";
                 mRecorder = new MediaRecorder();
                 mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                 mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
@@ -275,9 +274,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
                 }
                 mRecorder.start();
                 Toast.makeText(context, "Recording Started", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
+            } else {
                 RequestPermissions();
             }
         });
@@ -314,7 +311,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
             }
         });
         stopplay.setOnClickListener(v -> {
-            if(mPlayer!=null) {
+            if (mPlayer != null) {
                 mPlayer.release();
                 mPlayer = null;
                 stopbtn.setEnabled(false);
@@ -333,7 +330,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
             RequestBody fileBody = RequestBody.create(mediaType, file);
             fileParts[0] = MultipartBody.Part.createFormData("voiceData", file.getName(), fileBody);
             Log.d("filePath", "File Path: " + fileParts);//115741037999
-            if(ConnectivityChecker.checker(context)){
+            if (ConnectivityChecker.checker(context)) {
                 Call<String> call = RetrofitClientFile.getInstance().getApi().patientAudioVitalData(
                         SharedPrefManager.getInstance(context).getUser().getAccessToken(),
                         SharedPrefManager.getInstance(context).getUser().getUserid().toString(),
@@ -345,7 +342,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
                 call.enqueue(new Callback<String>() {
                     @Override
                     public void onResponse(Call<String> call, Response<String> response) {
-                        if(response.isSuccessful()){
+                        if (response.isSuccessful()) {
                             Toast.makeText(context, response.body(), Toast.LENGTH_LONG).show();
                             btnSave.setEnabled(false);
                             popupWindow.dismiss();
@@ -369,27 +366,30 @@ public class InputVital extends Fragment implements View.OnClickListener {
         }
         return type;
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case REQUEST_AUDIO_PERMISSION_CODE:
-                if (grantResults.length> 0) {
+                if (grantResults.length > 0) {
                     boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean permissionToStore = grantResults[1] ==  PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
                     if (permissionToRecord && permissionToStore) {
                         Toast.makeText(context, "Permission Granted", Toast.LENGTH_LONG).show();
                     } else {
-                        Toast.makeText(context,"Permission Denied",Toast.LENGTH_LONG).show();
+                        Toast.makeText(context, "Permission Denied", Toast.LENGTH_LONG).show();
                     }
                 }
                 break;
         }
     }
+
     private boolean CheckPermissions() {
         int result = ContextCompat.checkSelfPermission(context, WRITE_EXTERNAL_STORAGE);
         int result1 = ContextCompat.checkSelfPermission(context, RECORD_AUDIO);
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
+
     private void RequestPermissions() {
         ActivityCompat.requestPermissions(getActivity(), new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
     }
@@ -400,14 +400,16 @@ public class InputVital extends Fragment implements View.OnClickListener {
             mListener.onFragmentInteraction(uri);
         }
     }
-    public void removeRow(int id){
+
+    public void removeRow(int id) {
         try {
             //vitalLists1.remove(id);
             //rvAddVital.setAdapter(new AddVitalAdp(context, vitalLists1));
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
@@ -415,6 +417,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
             getFragmentManager().beginTransaction().detach(this).attach(this).commit();
         }
     }
+
     @Override
     public void onAttach(Context ctx) {
         super.onAttach(ctx);
@@ -425,6 +428,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
                     + " must implement OnFragmentInteractionListener");
         }*/
     }
+
     private void saveVitals(int drId) {
         Utils.showRequestDialog(context);
         Log.v("hitApi:", "http://182.156.200.179:201/api/Prescription/saveVitals");
@@ -477,14 +481,15 @@ public class InputVital extends Fragment implements View.OnClickListener {
                     }
                 });
     }
-    private void bind(){
+
+    private void bind() {
         Utils.showRequestDialog(context);
-        Call<List<VitalChart>> call=RetrofitClient.getInstance().getApi().getVitalChart(SharedPrefManager.getInstance(context).getUser().getAccessToken(), SharedPrefManager.getInstance(context).getUser().getUserid().toString(), SharedPrefManager.getInstance(context).getPid(), format1.format(today), SharedPrefManager.getInstance(context).getHeadID(), SharedPrefManager.getInstance(context).getSubDept().getId(), SharedPrefManager.getInstance(context).getUser().getUserid());
+        Call<List<VitalChart>> call = RetrofitClient.getInstance().getApi().getVitalChart(SharedPrefManager.getInstance(context).getUser().getAccessToken(), SharedPrefManager.getInstance(context).getUser().getUserid().toString(), SharedPrefManager.getInstance(context).getPid(), format1.format(today), SharedPrefManager.getInstance(context).getHeadID(), SharedPrefManager.getInstance(context).getSubDept().getId(), SharedPrefManager.getInstance(context).getUser().getUserid());
         call.enqueue(new Callback<List<VitalChart>>() {
             @Override
             public void onResponse(Call<List<VitalChart>> call, Response<List<VitalChart>> response) {
-                if(response.isSuccessful()){
-                    if(Objects.requireNonNull(response.body()).size()>0){
+                if (response.isSuccessful()) {
+                    if (Objects.requireNonNull(response.body()).size() > 0) {
                         rvVitalDisplay.setAdapter(new ShowVitalAdp(context, response.body()));
                     }
                 }
@@ -497,6 +502,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
             }
         });
     }
+
     @Override
     public void onDetach() {
         super.onDetach();
@@ -506,7 +512,7 @@ public class InputVital extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
-        if(view.getId()==R.id.txtDate){
+        if (view.getId() == R.id.txtDate) {
             DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), R.style.DialogTheme,
                     (view1, year, monthOfYear, dayOfMonth) -> {
                         mYear = year;
@@ -515,25 +521,25 @@ public class InputVital extends Fragment implements View.OnClickListener {
                         date = year + "/" + (monthOfYear + 1) + "/" + dayOfMonth;
                         today.setDate(dayOfMonth);
                         today.setMonth(monthOfYear);
-                        today.setYear(year-1900);
+                        today.setYear(year - 1900);
                         txtDate.setText(Utils.formatDate(date));
                         bind();
                     }, mYear, mMonth, mDay);
             datePickerDialog.show();
             datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
-        } else if(view.getId()==R.id.txtTime){
+        } else if (view.getId() == R.id.txtTime) {
             TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), R.style.DialogTheme, (timePicker, i, i1) -> {
-                mHour=i;
-                mMinute=i1;
+                mHour = i;
+                mMinute = i1;
                 c.set(Calendar.HOUR_OF_DAY, mHour);
                 c.set(Calendar.MINUTE, mMinute);
                 today.setHours(mHour);
                 today.setMinutes(mMinute);
                 txtTime.setText(format2.format(today));
-            },mHour,mMinute,false);
+            }, mHour, mMinute, false);
             timePickerDialog.updateTime(today.getHours(), today.getMinutes());
             timePickerDialog.show();
-        } else if(view.getId()==R.id.txtVoice){
+        } else if (view.getId() == R.id.txtVoice) {
             recordingPopup();
         }
     }
